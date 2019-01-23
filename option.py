f6251ad8c1319182da9c58f5f64bda7c88f92c3d
Noel <36836739+NoelShin@users.noreply.github.com>
2019-01-23 13:44:11 +0900

    Update option.py

--- option.py
+++ option.py
@@ -12,7 +12,7 @@ class BaseOption(object):
         self.parser.add_argument('--batch_size', type=int, default=1, help='the number of batch_size')
         self.parser.add_argument('--dataset_name', type=str, default='Cityscapes', help='[Cityscapes, Custom]')
         self.parser.add_argument('--C_condition', action='store_true', default=False, help='whether to condition Critic')
-        self.parser.add_argument('--C_norm', action='store_true', default=False,
+        self.parser.add_argument('--C_norm', action='store_true', default=True,
                                  help='whether to use norm layer in Critic')
         self.parser.add_argument('--GAN_type', type=str, default='WGAN_GP', help='[LSGAN, WGAN_GP]')
         self.parser.add_argument('--growth_rate', type=int, default=256)
@@ -23,13 +23,13 @@ class BaseOption(object):
         self.parser.add_argument('--n_dense_layer', type=int, default=6, help='how many dense layers in a RDB')
         self.parser.add_argument('--n_downsample', type=int, default=4,
                                  help='how many times you want downsample the original data')
-        self.parser.add_argument('--n_residual', type=int, default=16, help='the number of residual (dense) blocks')
+        self.parser.add_argument('--n_residual', type=int, default=8, help='the number of residual (dense) blocks')
         self.parser.add_argument('--n_workers', type=int, default=2, help='how many threads you want to use')
         self.parser.add_argument('--norm_type', type=str, default='InstanceNorm2d',
                                  help='[BatchNorm2d, InstanceNorm2d, PixelNorm]')
         self.parser.add_argument('--pad_type', type=str, default='reflection',
                                  help='[reflection, replication, zero]')
-        self.parser.add_argument('--RDB_ch', type=int, default=256)
+        self.parser.add_argument('--RDB_ch', type=int, default=1024)
         self.parser.add_argument('--trans_unit', type=str, default='RDB',
                                  help='Unit you want to use for image translation. "RB" for residual block, "RDB" for Residual dense block')
         self.parser.add_argument('--use_boundary_map', action='store_true', default=True,
@@ -51,13 +51,14 @@ class TrainOption(BaseOption):
         self.parser.add_argument('--beta1', type=float, default=0.0)
         self.parser.add_argument('--beta2', type=float, default=0.9)
         self.parser.add_argument('--display_freq', type=int, default=500)
+        self.parser.add_argument('--drift_lambda', type=float, default=0.001)
         self.parser.add_argument('--C_act', type=str, default='leaky_relu', help='which activation to use in Critic')
         self.parser.add_argument('--C_act_negative_slope', type=float, default=0.2, help='negative slope of C_act')
         self.parser.add_argument('--epoch_decay', type=int, default=100, help='when to start decay the lr')
         self.parser.add_argument('--eps', type=float, default=1e-8)
         self.parser.add_argument('--FM', action='store_true', default=True, help='switch for feature matching loss')
         self.parser.add_argument('--FM_criterion', type=str, default='L1', help='[L1, MSE]')
-        self.parser.add_argument('--FM_lambda', type=int, default=10, help='weight for feature matching loss')
+        self.parser.add_argument('--FM_lambda', type=int, default=1, help='weight for feature matching loss')
         self.parser.add_argument('--flip', action='store_true', default=True, help='switch for flip input data')
         self.parser.add_argument('--fan_mode', type=str, default='fan_in', help='He init keyword. Choose among [fan_in, fan_out]')
         self.parser.add_argument('--G_act', type=str, default='relu', help='which activation to use in Generator')

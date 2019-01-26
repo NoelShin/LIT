c32766764f37d7a06bc9275090ea8d7c8858605c
Noel <36836739+NoelShin@users.noreply.github.com>
2019-01-26 20:14:13 +0900

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -8,38 +8,39 @@ from PIL import Image
 
 
 class CustomDataset(torch.utils.data.Dataset):
-    def __init__(self, opt, lod):
+    def __init__(self, opt, lod=None):
         super(CustomDataset, self).__init__()
-        self.opt = opt
-        self.lod = lod
-        dataset_dir = os.path.join('./datasets', opt.dataset_name)
+        dataset_name = opt.dataset_name
+        is_train = opt.is_train
         format = opt.format
+
+        dataset_dir = os.path.join('./datasets', dataset_name)
 
         assert os.path.isdir(dataset_dir), print(
             "{} does not exist. Please check your dataset directory.".format(dataset_dir))
 
-        if opt.dataset_name == 'Cityscapes':
-            if opt.is_train:
+        if dataset_name == 'Cityscapes':
+            if is_train:
                 self.label_path_list = sorted(
                     glob.glob(os.path.join(dataset_dir, 'Train', 'Input', 'LabelMap', '*.' + format)))
                 self.instance_path_list = sorted(
                     glob.glob(os.path.join(dataset_dir, 'Train', 'Input', 'InstanceMap', '*.' + format)))
                 self.target_path_list = sorted(glob.glob(os.path.join(dataset_dir, 'Train', 'Target', '*.' + format)))
 
-            elif not opt.is_train:
+            elif not is_train:
                 self.label_path_list = sorted(
                     glob.glob(os.path.join(dataset_dir, 'Test', 'Input', 'LabelMap', '*.' + format)))
                 self.instance_path_list = sorted(
                     glob.glob(os.path.join(dataset_dir, 'Test', 'Input', 'InstanceMap', '*.' + format)))
                 self.target_path_list = sorted(glob.glob(os.path.join(dataset_dir, 'Test', 'Target', '*.' + format)))
 
-        elif opt.dataset_name == 'Custom':
-            if opt.is_train:
+        elif dataset_name == 'Custom':
+            if is_train:
                 self.label_path_list = sorted(
                     glob.glob(os.path.join(dataset_dir, 'Train', 'Input', 'LabelMap', '*.' + format)))
                 self.target_path_list = sorted(glob.glob(os.path.join(dataset_dir, 'Train', 'Target', '*.' + format)))
 
-            elif not opt.is_train:
+            elif not is_train:
                 self.label_path_list = sorted(
                     glob.glob(os.path.join(dataset_dir, 'Test', 'Input', 'LabelMap', '*.' + format)))
                 self.target_path_list = sorted(glob.glob(os.path.join(dataset_dir, 'Test', 'Target', '*.' + format)))
@@ -47,18 +48,26 @@ class CustomDataset(torch.utils.data.Dataset):
         else:
             raise NotImplementedError("Please check dataset_name. It should be in ['Cityscapes', 'Custom'].")
 
+        self.condition = opt.C_condition
+        self.dataset_name = dataset_name
+        self.flip = opt.flip
+        self.min_image_size = opt.min_image_size
+        self.image_size = opt.image_size
+        self.is_train = is_train
+        self.lod = lod
+
     def get_transform(self, normalize=True, lod_size=False):
         transform_list = []
 
         if not lod_size:
-            transform_list += [transforms.Resize(self.opt.image_size, interpolation=Image.NEAREST)]
+            transform_list += [transforms.Resize(self.image_size, interpolation=Image.NEAREST)]
 
         elif lod_size:
-            transform_list += [transforms.Resize((2 ** int((np.log2(self.opt.min_image_size[0]) + self.lod)),
-                                                  2 ** int(np.log2(self.opt.min_image_size[1]) + self.lod)),
+            transform_list += [transforms.Resize((2 ** int((np.log2(self.min_image_size[0]) + self.lod)),
+                                                  2 ** int(np.log2(self.min_image_size[1]) + self.lod)),
                                                  interpolation=Image.NEAREST)]
 
-        if self.opt.is_train and self.coin:
+        if self.is_train and self.coin:
             transform_list.append(transforms.Lambda(lambda x: self.__flip(x)))
 
         transform_list.append(transforms.ToTensor())
@@ -83,7 +92,7 @@ class CustomDataset(torch.utils.data.Dataset):
         return x.transpose(Image.FLIP_LEFT_RIGHT)
 
     def encode_input(self, label_tensor, instance_tensor=None):
-        if self.opt.dataset_name == 'Cityscapes':
+        if self.dataset_name == 'Cityscapes':
             max_label_index = 35
             shape = label_tensor.shape
             one_hot_shape = (max_label_index, shape[1], shape[2])
@@ -96,12 +105,12 @@ class CustomDataset(torch.utils.data.Dataset):
 
             return input_tensor
 
-        elif self.opt.dataset_name == 'Custom':
+        elif self.dataset_name == 'Custom':
             return label_tensor
 
     def __getitem__(self, index):
-        if self.opt.dataset_name == 'Cityscapes':
-            if self.opt.flip:
+        if self.dataset_name == 'Cityscapes':
+            if self.flip:
                 self.coin = random.random() > 0.5
 
             label_array = Image.open(self.label_path_list[index])
@@ -117,14 +126,14 @@ class CustomDataset(torch.utils.data.Dataset):
 
             data_dict = {'input_tensor': input_tensor, 'target_tensor': target_tensor}
 
-            if self.opt.C_condition:
-                D_label_tensor = self.get_transform(normalize=False, lod_size=True)(label_array) * 255.0
-                D_instance_tensor = self.get_transform(normalize=False, lod_size=True)(instance_array)
-                D_input_tensor = self.encode_input(D_label_tensor, D_instance_tensor)
+            if self.condition:
+                C_label_tensor = self.get_transform(normalize=False, lod_size=True)(label_array) * 255.0
+                C_instance_tensor = self.get_transform(normalize=False, lod_size=True)(instance_array)
+                C_input_tensor = self.encode_input(C_label_tensor, C_instance_tensor)
 
-                data_dict.update({'D_input_tensor': D_input_tensor})
+                data_dict.update({'C_input_tensor': C_input_tensor})
 
-        elif self.opt.dataset_name == 'Custom':
+        elif self.dataset_name == 'Custom':
             label_array = Image.open(self.label_path_list[index])
             label_tensor = self.get_transform(normalize=True, lod_size=False)(label_array)
 
@@ -135,11 +144,11 @@ class CustomDataset(torch.utils.data.Dataset):
 
             data_dict = {'input_tensor': input_tensor, 'target_tensor': target_tensor}
 
-            if self.opt.C_condition:
-                D_label_tensor = self.get_transform(normalize=True, lod_size=True)(label_array)
-                D_input_tensor = self.encode_input(D_label_tensor)
+            if self.condition:
+                C_label_tensor = self.get_transform(normalize=True, lod_size=True)(label_array)
+                C_input_tensor = self.encode_input(C_label_tensor)
 
-                data_dict.update({'D_input_tensor': D_input_tensor})
+                data_dict.update({'D_input_tensor': C_input_tensor})
 
         else:
             raise NotImplementedError("Please check dataset_name. It should be in ['Cityscapes', 'Custom'].")

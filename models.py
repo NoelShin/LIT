e849e8106def91bb4173afcd6a1cca7ceefe6fa5
Noel <36836739+NoelShin@users.noreply.github.com>
2019-02-05 16:45:19 +0900

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from functools import partial
+import torch
 import torch.nn as nn
 import torchvision
 from networks.base_network import BaseNetwork
@@ -285,38 +286,45 @@ class ProgressiveUNetGenerator(BaseGenerator):
 
         n_ch = opt.n_gf
         n_downsample = opt.n_downsample
-        layer_first = [pad(1), nn.Conv2d(opt.input_ch, n_ch, kernel_size=3, padding=0, stride=1)]
+        layer_first = [pad(3), nn.Conv2d(opt.input_ch, n_ch, kernel_size=7, padding=0, stride=1)]
+        layer_first += self.add_norm_act_layer(norm, n_ch=n_ch, act=act)
         self.layer_first = nn.Sequential(*layer_first)
 
         RGB_layer = [[pad(1), nn.Conv2d(n_ch, opt.output_ch, kernel_size=3, padding=0, stride=1)]]
         RGB_layer[-1].append(nn.Tanh()) if opt.tanh else None
 
-        for i in range(opt.n_downsample):
-            encoder_layer = self.add_norm_act_layer(norm, n_ch=n_ch, act=act)
-            encoder_layer += [pad(1), nn.Conv2d(n_ch, 2 * n_ch, kernel_size=3, padding=0, stride=2)]
-            encoder_layer += self.add_norm_act_layer(norm, n_ch=2 * n_ch, act=act) if i == n_downsample - 1 else []
+        gate_layer = [[ChannelAttentionLayer(n_ch)]]
+        for i in range(n_downsample):
+            encoder_layer = [pad(1), nn.Conv2d(n_ch, 2 * n_ch, kernel_size=3, padding=0, stride=2)]
+            encoder_layer += self.add_norm_act_layer(norm, n_ch=2 * n_ch, act=act)
+            gate_layer += [[ChannelAttentionLayer(2 * n_ch)]] if i != n_downsample - 1 else []
 
             RGB_layer += [[pad(1), nn.Conv2d(2 * n_ch, opt.output_ch, kernel_size=3, padding=0, stride=1)]]
             RGB_layer[-1].append(nn.Tanh()) if opt.tanh else None
 
             decoder_layer = [nn.ConvTranspose2d(2 * n_ch, n_ch, kernel_size=3, padding=1, stride=2, output_padding=1)]
-            decoder_norm_act_layer = self.add_norm_act_layer(norm, n_ch=n_ch, act=act)
+            decoder_layer += self.add_norm_act_layer(norm, n_ch=n_ch, act=act)
 
-            enhance_layer = self.get_enhance_layer(opt, n_ch, act, norm, pad)
+            enhance_layer = ResidualNetwork(1, n_ch, act, norm, pad)
 
             n_ch *= 2
 
             setattr(self, 'Down_layer_{}'.format(i), nn.Sequential(*encoder_layer))
             setattr(self, 'Up_layer_{}'.format(i), nn.Sequential(*decoder_layer))
-            setattr(self, 'Up_norm_act_layer_{}'.format(i), nn.Sequential(*decoder_norm_act_layer))
             setattr(self, 'Enhance_layer_{}'.format(i), nn.Sequential(*enhance_layer))
 
         n_RGB = len(RGB_layer)
         for i in range(n_RGB): # 0 1 2 3 4
             setattr(self, 'RGB_layer_{}'.format(n_RGB - 1 - i), nn.Sequential(*RGB_layer[i]))
 
+        n_gate = len(gate_layer)
+        for i in range(n_gate):
+            setattr(self, 'Gate_layer_{}'.format(i), nn.Sequential(*gate_layer[i]))
+
         self.translator = self.get_trans_network(opt, act, norm, pad, input_ch=n_ch)
-        self.n_downsample = opt.n_downsample
+        self.n_downsample = n_downsample
+        self.n_gate = n_gate
+        self.n_RGB = n_RGB
         self.apply(partial(self.init_weights, type=opt.init_type, mode=opt.fan_mode, negative_slope=opt.negative_slope,
                            nonlinearity=opt.G_act))
         self.to_CUDA(opt.gpu_ids)
@@ -324,19 +332,23 @@ class ProgressiveUNetGenerator(BaseGenerator):
         print(self)
         print("the number of G parameters: ", sum(p.numel() for p in self.parameters() if p.requires_grad))
 
-    def forward(self, x, level):
+    def forward(self, x, level, level_in):
         results = [self.layer_first(x)]
         for i in range(self.n_downsample):
             results.append(getattr(self, 'Down_layer_{}'.format(i))(results[-1]))
 
         x = self.translator(results[-1])
-        for i in range(level):  # 0 1 2 3
-            x = results[self.n_downsample - 1 - i] + getattr(self, 'Up_layer_{}'.format(self.n_downsample - 1 - i))(x)
-            x = getattr(self, 'Up_norm_act_layer_{}'.format(i))(x)
-            x = getattr(self, 'Enhance_layer_{}'.format(self.n_downsample - 1 - i))(x)
+        out = getattr(self, 'RGB_layer_{}'.format(self.n_RGB))(x)
 
-        x = getattr(self, 'RGB_layer_{}'.format(level))(x)
-        return x
+        for i in range(level):
+            x = getattr(self, 'Gate_layer_{}'.format(self.n_gate - i))(results[self.n_downsample - 1 - i])\
+                + getattr(self, 'Up_layer_{}'.format(self.n_downsample - 1 - i))(x)
+            x = getattr(self, 'Enhance_layer_{}'.format(self.n_downsample - 1 - i))(x)
+            y = getattr(self, 'RGB_layer_{}'.format(self.n_RGB - 1 - i))(x)
+            out = nn.functional.interpolate(out, scale_factor=2, mode='nearest')
+            out = torch.lerp(out, y, level_in - level)
+
+        return out
 
 
 class UNetGenerator(BaseGenerator):

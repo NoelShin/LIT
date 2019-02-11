c8009a44a39de143d381ab4f6147521058890fe1
Noel <36836739+NoelShin@users.noreply.github.com>
2019-02-11 22:39:22 +0900

    Update residual_dense_block.py

--- networks/modules/residual_dense_block.py
+++ networks/modules/residual_dense_block.py
@@ -1,17 +1,22 @@
 import torch
 import torch.nn as nn
 from .base_module import BaseModule
+from .residual_channel_attention_block import ChannelAttentionLayer
 
 
 class DenseLayer(BaseModule):
-    def __init__(self, n_ch, growth_rate, kernel_size=3, act='relu', norm='InstanceNorm2d', pad='reflection'):
+    def __init__(self, n_ch, growth_rate, kernel_size=3, act='relu', norm='InstanceNorm2d', pad='reflection',
+                 pre_activation=False):
         super(DenseLayer, self).__init__()
         act = self.get_act_layer(act, inplace=True) if isinstance(act, str) else act
         norm = self.get_norm_layer(norm) if isinstance(norm, str) else norm
         pad = self.get_pad_layer(pad) if isinstance(pad, str) else pad
 
-        layer = [pad(1), nn.Conv2d(n_ch, growth_rate, kernel_size=kernel_size, padding=0, stride=1, bias=False)]
-        layer += self.add_norm_act_layer(norm, n_ch=growth_rate, act=act)
+        if pre_activation:
+            layer = [norm(n_ch), act, pad(1), nn.Conv2d(n_ch, growth_rate, kernel_size=kernel_size, bias=False)]
+
+        else:
+            layer = [pad(1), nn.Conv2d(n_ch, growth_rate, kernel_size=kernel_size, bias=False), norm(growth_rate), act]
 
         self.layer = nn.Sequential(*layer)
 
@@ -25,16 +30,34 @@ class DenseLayer(BaseModule):
 class ResidualDenseBlock(BaseModule):
     def __init__(self, n_ch, growth_rate, n_dense_layers, act, norm, pad, kernel_size=3):
         super(ResidualDenseBlock, self).__init__()
-        init_n_ch = n_ch
+        init_ch = n_ch
 
         block = []
         for _ in range(n_dense_layers):
             block += [DenseLayer(n_ch, growth_rate, kernel_size=kernel_size, act=act, norm=norm, pad=pad)]
             n_ch += growth_rate
-        block += [nn.Conv2d(n_ch, init_n_ch, kernel_size=1, padding=0, stride=1, bias=False)]  # local feature fusion
+        block += [nn.Conv2d(n_ch, init_ch, kernel_size=1, bias=False)]  # local feature fusion
         self.block = nn.Sequential(*block)
 
     def forward(self, x):
         x = x + self.block(x)  # local residual learning
 
+        return x
+
+
+class ResidualChannelAttentionDenseBlock(BaseModule):
+    def __init__(self, n_ch, growth_rate, n_dense_layers, act, norm, pad, kernel_size=3):
+        super(ResidualChannelAttentionDenseBlock, self).__init__()
+        init_ch = n_ch
+
+        block = []
+        for _ in range(n_dense_layers):
+            block += [DenseLayer(n_ch, growth_rate, kernel_size=kernel_size, act=act, norm=norm, pad=pad)]
+            n_ch += growth_rate
+        block += [nn.Conv2d(n_ch, init_ch, kernel_size=3), norm(init_ch), act]
+        block += [ChannelAttentionLayer(init_ch)]
+        self.block = nn.Sequential(*block)
+
+    def forward(self, x):
+        x = x + self.block(x)
         return x

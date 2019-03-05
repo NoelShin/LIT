bf34c91600fdef3a85a8896789064741f94f5e5c
Noel <36836739+NoelShin@users.noreply.github.com>
2019-03-05 18:51:41 +0900

    Update dense_modules.py

--- networks/dense_modules.py
+++ networks/dense_modules.py
@@ -27,31 +27,35 @@ class DenseLayer(nn.Module):
 
 
 class DenseBlock(nn.Module):
-    def __init__(self, n_ch, growth_rate, n_dense_layers, efficient=True):
+    def __init__(self, n_ch, growth_rate, n_dense_layers, efficient=True, block_weight=False):
         super(DenseBlock, self).__init__()
+        self.add_module('Entry', nn.Conv2d(nn.Conv2d(n_ch, growth_rate, kernel_size=1)))
         for i in range(n_dense_layers):
             self.add_module('Dense_layer_{}'.format(i), DenseLayer(n_ch + growth_rate * i, growth_rate,
                                                                    efficient=efficient))
+        self.add_module('Exit', nn.Conv2d(nn.Conv2d(growth_rate, n_ch, kernel_size=1)))
+        setattr(self, 'BlockWeight', nn.Parameter(torch.tensor(1.0))) if block_weight else None
+        self.block_weight = block_weight
         self.n_dense_layers = n_dense_layers
 
     def forward(self, x):
-        features = [x]
+        features = [getattr(self, 'Entry')(x)]
         for i in range(self.n_dense_layers):
             features += [getattr(self, 'Dense_layer_{}'.format(i))(*features)]
-        return features[-1]
+
+        if self.block_weight:
+            return getattr(self, 'BlockWeight') * getattr(self, 'Exit')(features[-1])
+        else:
+            return getattr(self, 'Exit')(features[-1])
 
 
 class DenseNetwork(nn.Module):
-    def __init__(self, n_blocks, n_ch, growth_rate, n_dense_layers, efficient=True):
+    def __init__(self, n_blocks, n_ch, growth_rate, n_dense_layers, efficient=True, block_weight=False):
         super(DenseNetwork, self).__init__()
         for i in range(n_blocks):
             self.add_module('Dense_block_{}'.format(i), DenseBlock(n_ch, growth_rate, n_dense_layers,
-                                                                   efficient=efficient))
-        self.add_module('GFF', nn.Conv2d((n_blocks + 1) * growth_rate, 1024, 1))
+                                                                   efficient=efficient, block_weight=block_weight))
         self.n_blocks = n_blocks
-
-    def function(self, *features):
-        return getattr(self, 'GFF')(torch.cat(features, dim=1))
 
     def forward(self, x):
         features = [x]
